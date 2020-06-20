077017667441a422c32fb22298123d6c8e50cb97
David Guzmán <da.guzman@outlook.com>
2020-06-20 11:29:24 -0500

    now prints basic information of connected device

--- abacusExample.py
+++ abacusExample.py
@@ -7,67 +7,80 @@ print("********************")
 print("1. Find devices and establish a connection\n")
 
 ports, n = abacus.findDevices()
-print(ports)
 
-port = list(ports.values())[0] #get first available device
+if n==0:
+    print("\nNo valid devices were found. Closing.")
+    
+else:
+    print("\nAvailable valid devices:")
+    print(ports)
 
-abacus.open(port)			#open connection with device
-#abacus.getAllCounters(port)		#read all counter's data from device
-#abacus.setSetting(port,"sampling", 5)	#set a single setting in device
+    port = list(ports.values())[0] #get first available device
 
-print("\n********************")
-print("2. Read device settings\n")
+    abacus.open(port)			#open connection with device
 
-#Example of reading all device settings
-settings = abacus.getAllSettings(port)
-print("Settings read from device, using getAllSettings method:")
-print("   settings =",settings)
+    print("\nConnected to the following device:")
+    name = abacus.getIdn(port)
+    numchannels = abacus.getChannelsFromName(name)
+    resolution = abacus.getResolutionFromName(name)
+    print("   device port =",port)
+    print("   device name =",name)
+    print("   number of channels =",numchannels)
+    print("   resolution =",resolution,"ns")
 
-#Examples reading single settings
-value = abacus.getSetting(port,"delay_A")
-print("current delay_A=",value,"ns")
-value = abacus.getSetting(port,"delay_B")
-print("current delay_B=",value,"ns")
-value = abacus.getSetting(port,"sleep_A")
-print("current sleep_A=",value,"ns")
-value = abacus.getSetting(port,"sleep_B")
-print("current sleep_B=",value,"ns")
-value = abacus.getSetting(port,"coincidence_window")
-print("current coincidence_window=",value,"ns")
-value = abacus.getSetting(port,"sampling")
-print("current sampling=",value,"ms")
+    print("\n********************")
+    print("2. Read device settings\n")
 
-print("\n********************")
-print("3. Write device settings\n")
+    #Example of reading all device settings
+    settings = abacus.getAllSettings(port)
+    print("Settings read from device, using getAllSettings method:")
+    print("   settings =",settings)
 
-#Example of writing a new setting value
-abacus.setSetting(port,"sampling", 2000)    #set sampling=2000ms
-value = abacus.getSetting(port,"sampling")  #read sampling
-print("current sampling=",value,"ms")
+    #Examples reading single settings
+    value = abacus.getSetting(port,"delay_A")
+    print("current delay_A=",value,"ns")
+    value = abacus.getSetting(port,"delay_B")
+    print("current delay_B=",value,"ns")
+    value = abacus.getSetting(port,"sleep_A")
+    print("current sleep_A=",value,"ns")
+    value = abacus.getSetting(port,"sleep_B")
+    print("current sleep_B=",value,"ns")
+    value = abacus.getSetting(port,"coincidence_window")
+    print("current coincidence_window=",value,"ns")
+    value = abacus.getSetting(port,"sampling")
+    print("current sampling=",value,"ms")
 
-abacus.setSetting(port,"delay_B", 20)	    #set delay_B=20ns
-value = abacus.getSetting(port,"delay_B")
-print("current delay_B=",value,"ns")
+    print("\n********************")
+    print("3. Write device settings\n")
 
-abacus.setSetting(port,"coincidence_window", 50)	    #set coincidence_window=50ns
-value = abacus.getSetting(port,"coincidence_window")
-print("current coincidence_window=",value,"ns")
+    #Example of writing a new setting value
+    abacus.setSetting(port,"sampling", 2000)    #set sampling=2000ms
+    value = abacus.getSetting(port,"sampling")  #read sampling
+    print("current sampling=",value,"ms")
 
-print("waiting 2 seconds to complete the measurement")
-time.sleep(2); #wait sampling time (2s) to get a valid measurement
+    abacus.setSetting(port,"delay_B", 20)	    #set delay_B=20ns
+    value = abacus.getSetting(port,"delay_B")
+    print("current delay_B=",value,"ns")
 
-print("\n********************")
-print("4. Read measurements from device\n")
+    abacus.setSetting(port,"coincidence_window", 50)	    #set coincidence_window=50ns
+    value = abacus.getSetting(port,"coincidence_window")
+    print("current coincidence_window=",value,"ns")
 
-#Example of reading measurements from counters
-counters, counters_id = abacus.getAllCounters(port)
-print("Measurements read from device, using getAllCounters method:")
-print("   counters_id =",counters_id)
-print("   counters =",counters)
-print("   counters type=",type(counters))
-print("   counts in A:                    counters.getValue('A')  =",counters.getValue('A'))
-print("   coincidences between A and B:   counters.getValue('AB') =",counters.getValue('AB'))
+    print("waiting 2 seconds to complete the measurement")
+    time.sleep(2); #wait sampling time (2s) to get a valid measurement
 
-print("   counts in A and B:              counters.getValues({'A','B'}) =",counters.getValues({'A','B'}))
+    print("\n********************")
+    print("4. Read measurements from device\n")
 
-abacus.close(port)			#close connection with device
+    #Example of reading measurements from counters
+    counters, counters_id = abacus.getAllCounters(port)
+    print("Measurements read from device, using getAllCounters method:")
+    print("   counters_id =",counters_id)
+    print("   counters =",counters)
+    print("   counters type=",type(counters))
+    print("   counts in A:                    counters.getValue('A')  =",counters.getValue('A'))
+    print("   coincidences between A and B:   counters.getValue('AB') =",counters.getValue('AB'))
+
+    print("   counts in A and B:              counters.getValues({'A','B'}) =",counters.getValues({'A','B'}))
+
+    abacus.close(port)			#close connection with device
