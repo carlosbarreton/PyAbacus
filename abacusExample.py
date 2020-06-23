8281510fa0f4b50864f871c32b05de12485e2b1b
David Guzmán <da.guzman@outlook.com>
2020-06-23 16:31:55 -0500

    Add files via upload

--- abacusExample.py
+++ abacusExample.py
@@ -6,7 +6,7 @@ print("pyAbacus example")
 print("********************")
 print("1. Find devices and establish a connection\n")
 
-ports, n = abacus.findDevices()
+ports, n = abacus.findDevices() #required to scan devices before opening a connection
 
 if n==0:
     print("\nNo valid devices were found. Closing.")
@@ -15,16 +15,18 @@ else:
     print("\nAvailable valid devices:")
     print(ports)
 
-    port = list(ports.values())[0] #get first available device
+    mydevice = list(ports.keys())[0] #get first available device
 
-    abacus.open(port)			#open connection with device
+    abacus.open(mydevice)			#open connection with device
 
     print("\nConnected to the following device:")
-    name = abacus.getIdn(port)
-    numchannels = abacus.getChannelsFromName(name)
-    resolution = abacus.getResolutionFromName(name)
-    print("   device port =",port)
-    print("   device name =",name)
+    idnstring = abacus.getIdn(mydevice)
+    numchannels = abacus.getChannelsFromName(mydevice)
+    resolution = abacus.getResolutionFromName(mydevice)
+    physicalport = abacus.getPhysicalPort(mydevice)
+    print("   device name =",mydevice)
+    print("   device physical port =",physicalport)
+    print("   device identifier string =",idnstring)
     print("   number of channels =",numchannels)
     print("   resolution =",resolution,"ns")
 
@@ -32,38 +34,38 @@ else:
     print("2. Read device settings\n")
 
     #Example of reading all device settings
-    settings = abacus.getAllSettings(port)
+    settings = abacus.getAllSettings(mydevice)
     print("Settings read from device, using getAllSettings method:")
     print("   settings =",settings)
 
     #Examples reading single settings
-    value = abacus.getSetting(port,"delay_A")
+    value = abacus.getSetting(mydevice,"delay_A")
     print("current delay_A=",value,"ns")
-    value = abacus.getSetting(port,"delay_B")
+    value = abacus.getSetting(mydevice,"delay_B")
     print("current delay_B=",value,"ns")
-    value = abacus.getSetting(port,"sleep_A")
+    value = abacus.getSetting(mydevice,"sleep_A")
     print("current sleep_A=",value,"ns")
-    value = abacus.getSetting(port,"sleep_B")
+    value = abacus.getSetting(mydevice,"sleep_B")
     print("current sleep_B=",value,"ns")
-    value = abacus.getSetting(port,"coincidence_window")
+    value = abacus.getSetting(mydevice,"coincidence_window")
     print("current coincidence_window=",value,"ns")
-    value = abacus.getSetting(port,"sampling")
+    value = abacus.getSetting(mydevice,"sampling")
     print("current sampling=",value,"ms")
 
     print("\n********************")
     print("3. Write device settings\n")
 
     #Example of writing a new setting value
-    abacus.setSetting(port,"sampling", 2000)    #set sampling=2000ms
-    value = abacus.getSetting(port,"sampling")  #read sampling
+    abacus.setSetting(mydevice,"sampling", 2000)    #set sampling=2000ms
+    value = abacus.getSetting(mydevice,"sampling")  #read sampling
     print("current sampling=",value,"ms")
 
-    abacus.setSetting(port,"delay_B", 20)	    #set delay_B=20ns
-    value = abacus.getSetting(port,"delay_B")
+    abacus.setSetting(mydevice,"delay_B", 20)	    #set delay_B=20ns
+    value = abacus.getSetting(mydevice,"delay_B")
     print("current delay_B=",value,"ns")
 
-    abacus.setSetting(port,"coincidence_window", 50)	    #set coincidence_window=50ns
-    value = abacus.getSetting(port,"coincidence_window")
+    abacus.setSetting(mydevice,"coincidence_window", 50)	    #set coincidence_window=50ns
+    value = abacus.getSetting(mydevice,"coincidence_window")
     print("current coincidence_window=",value,"ns")
 
     print("waiting 2 seconds to complete the measurement")
@@ -73,7 +75,7 @@ else:
     print("4. Read measurements from device\n")
 
     #Example of reading measurements from counters
-    counters, counters_id = abacus.getAllCounters(port)
+    counters, counters_id = abacus.getAllCounters(mydevice)
     print("Measurements read from device, using getAllCounters method:")
     print("   counters_id =",counters_id)
     print("   counters =",counters)
@@ -83,4 +85,4 @@ else:
 
     print("   counts in A and B:              counters.getValues({'A','B'}) =",counters.getValues({'A','B'}))
 
-    abacus.close(port)			#close connection with device
+    abacus.close(mydevice)			#close connection with device
